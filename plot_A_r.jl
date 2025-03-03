15930f80067c63bc3d919737461aae7085c094f3
Siyuan Chen <chance.siyuan@gmail.com>
2025-03-03 20:32:58 +0800

    Integrated version

--- plot_A_r.jl
+++ plot_A_r.jl
@@ -1,73 +1,224 @@
 # using Pkg
 # Pkg.add("Bits")
 # Pkg.add("Plots")
+# Pkg.add("LaTeXStrings")
 using PauliPropagation
 using Plots
 using Random
+using LaTeXStrings
 
 function Plot_transition_point!(pic, circuit, observable, parameters; label="Data")
     #开始Pauli反向传播
     pauli_sum = propagate(circuit, observable, parameters)
+    print(pauli_sum)
     #提取最终Pauli项的系数值
     my_values = [x^2 for x in collect(values(pauli_sum.terms))]
     my_length = length(my_values)
     # println("sum_of_values=", sum(my_values)) #校验是否归一
     # println("my_length=", my_length)
     sorted_my_values = sort(my_values, rev=true)
-    range = 0:0.01:0.5
+    range = -1:0.01:0.2
     Alist = []
     for r in range
-        local A_values = [(i^r)*sorted_my_values[i] for i in 1:my_length]
+        local A_values = [(i^(1 + r))*sorted_my_values[i] for i in 1:my_length]
         sorted_A_values = sort(A_values, rev=true) # 从大到小排序
         A = sorted_A_values[1] #取最大值
         push!(Alist, A)
     end
     # 绘制 A vs r 的图形
-    scatter!(pic, range, Alist, label=label)
+    plot!(pic, range, Alist, label=label)
+end
+
+function noisy_su4circuit(nqubits::Integer, nlayers::Integer; topology=nothing,noise_rate::Real,noise_type::Integer)
+    
+"""
+noisy_su4circuit(nqubits::Integer, nlayers::Integer; topology=nothing)
+Create a circuit that consists of layers of SU(4) gates on a given topology and depolarizing noise layers on each qubit. 
+If no topology is specified, a bricklayer topology is used.
+
+"""
+    circuit::Vector{Gate} = []
+    if isnothing(topology)
+        topology = bricklayertopology(nqubits)
+    end
+    if noise_type == 1
+        append!(circuit, (FrozenGate(DepolarizingNoise(qind), noise_rate) for qind in 1:nqubits))
+        for nl in 1:nlayers
+            for pair in topology
+                appendSU4!(circuit, pair)
+            end
+            append!(circuit, (FrozenGate(DepolarizingNoise(qind), noise_rate)  for qind in 1:nqubits))
+        end
+    elseif noise_type == 2
+        append!(circuit, (FrozenGate(DephasingNoise(qind), noise_rate) for qind in 1:nqubits))
+        for nl in 1:nlayers
+            for pair in topology
+                appendSU4!(circuit, pair)
+            end
+        end
+            append!(circuit, (FrozenGate(DephasingNoise(qind), noise_rate)  for qind in 1:nqubits))
+    elseif noise_type == 3
+        append!(circuit, (FrozenGate(AmplitudeDampingNoise(qind), noise_rate) for qind in 1:nqubits))
+        for nl in 1:nlayers
+            for pair in topology
+                appendSU4!(circuit, pair)
+            end
+            append!(circuit, (FrozenGate(AmplitudeDampingNoise(qind), noise_rate)  for qind in 1:nqubits))
+        end
+    end
+
+    return circuit
+end
+
+function noisy_Clifford_T_circuit(nqubits::Integer, nlayers::Integer; topology=nothing,noise_rate::Real)
+    circuit::Vector{Gate} = []
+    if isnothing(topology)
+        topology = bricklayertopology(nqubits)
+    end
+    append!(circuit, (FrozenGate(DepolarizingNoise(qind), noise_rate)  for qind in 1:nqubits))
+    for nl in 1:nlayers
+        for pair in topology
+            if rand(Bool)
+                push!(circuit, CliffordGate(:CNOT, pair))
+            end
+        end
+        for indx in 1:nqubits
+            if rand(Bool)
+                push!(circuit, CliffordGate(:H, indx))
+            end
+            if rand(Bool)
+                push!(circuit, CliffordGate(:S, indx))
+            end
+        end
+        append!(circuit, (FrozenGate(PauliRotation(:Z, i),pi/4) for i in 1:nqubits))
+        append!(circuit, (FrozenGate(DepolarizingNoise(qind), noise_rate)  for qind in 1:nqubits))
+    end
+    return circuit
+end
+
+function one_layer_of_T_gate(nqubits::Integer;noise_rate::Real,noise_type::Integer)
+    circuit::Vector{Gate} = []
+    append!(circuit,(FrozenGate(PauliRotation(:Z, i),pi/4) for i in 1:nqubits))
+    if noise_type == 1
+        append!(circuit, (FrozenGate(DepolarizingNoise(qind), noise_rate)  for qind in 1:nqubits))
+    elseif noise_type == 2
+        append!(circuit, (FrozenGate(DephasingNoise(qind), noise_rate)  for qind in 1:nqubits))
+    elseif noise_type == 3
+        append!(circuit, (FrozenGate(AmplitudeDampingNoise(qind), noise_rate)  for qind in 1:nqubits))
+    end
+    return circuit
 end
 
 
-nqubits = 10
-##############################################################################
-#生成随机一排2-qubit unitary拼成的线路
-Random.seed!(1234)
-nlayers = 1
-topology = bricklayertopology(nqubits; periodic=true)
-circuit1 = su4circuit(nqubits, nlayers; topology=topology)
-dt = 3.14159265358979323846
-nparams = countparameters(circuit1)
-parameters1 = rand(nparams) * dt
-#确定最终要测量的Pauli算符
-observable1 = PauliSum(nqubits)
-add!(observable1, [:X for i in 1:nqubits], [i for i in 1:nqubits], 1.0)
-##############################################################################
-#生成另一排2-qubit unitary拼成的线路
-Random.seed!(5678)
-circuit2 = su4circuit(nqubits, nlayers; topology=topology)
-parameters2 = rand(nparams) * dt
-#确定最终要测量的Pauli算符
-observable2 = PauliSum(nqubits)
-add!(observable2, [:X for i in 1:nqubits], [i for i in 1:nqubits], 1.0)
-##############################################################################
-#生成一排T gate的线路
-circuit3::Vector{Gate} = []
-append!(circuit3,(PauliRotation(:Z, i) for i in 1:nqubits))
-parameters3 = [pi/4 for i in 1:nqubits]
-#确定最终要测量的Pauli算符
-observable3 = PauliSum(nqubits)
-add!(observable3, [:X for i in 1:nqubits], [i for i in 1: nqubits], 1.0)
-##############################################################################
-# Create an empty plot
-pic = plot(title="A vs r", xlabel="r", ylabel="A", yscale=:log10)
-# Plot the first dataset
-Plot_transition_point!(pic, circuit1, observable1, parameters1; label="random circuit 1")
-# Plot the second dataset
-Plot_transition_point!(pic, circuit2, observable2, parameters2; label="random circuit 2")
-# Plot the Third dataset
-Plot_transition_point!(pic, circuit3, observable3, parameters3; label="T-layer circuit")
 
-# Display the plot
+Random.seed!(1234)
+global nqubits = 8
+global topology = bricklayertopology(nqubits; periodic=true)
+global dt = 3.14159265358979323846
+global nlayers = 10
+##############################################################################
+# # Create an empty plot
+# pic1 = plot(title="T+Depolarization noise", xlabel=L"\alpha", ylabel=L"C(\alpha)", yscale=:log10,legend=:outerright)
+# #确定最终要测量的Pauli算符
+# observable3 = PauliSum(nqubits)
+# parameters3 =[]
+# add!(observable3, [:X for i in 1:nqubits], [i for i in 1: nqubits], 1.0)
+# for noise_rate in 0:0.02:0.2
+#     #生成一排T gate的线路
+#     circuit3=one_layer_of_T_gate(nqubits;noise_rate,noise_type = 1)
+#     # # Plot the Third dataset
+#     Plot_transition_point!(pic1, circuit3, observable3, parameters3; label="p= $noise_rate")
+# end
+# display(pic1)
+##############################################################################
+# # Create an empty plot
+# pic2 = plot(title="T+Dephasing noise", xlabel=L"\alpha", ylabel=L"C(\alpha)", yscale=:log10,legend=:outerright)
+# #确定最终要测量的Pauli算符
+# observable3 = PauliSum(nqubits)
+# parameters3 =[]
+# add!(observable3, [:X for i in 1:nqubits], [i for i in 1: nqubits], 1.0)
+# for noise_rate in 0:0.02:0.2
+#     #生成一排T gate的线路
+#     circuit3=one_layer_of_T_gate(nqubits;noise_rate,noise_type = 2)
+#     # # Plot the Third dataset
+#     Plot_transition_point!(pic2, circuit3, observable3, parameters3; label="p= $noise_rate")
+# end
+# display(pic2)
+##############################################################################
+# # Create an empty plot
+# pic3 = plot(title="T+Depolarizing noise", xlabel=L"\alpha", ylabel=L"C(\alpha)", yscale=:log10,legend=:outerright)
+# #确定最终要测量的Pauli算符
+# observable3 = PauliSum(nqubits)
+# parameters3 =[]
+# add!(observable3, [:X for i in 1:nqubits], [i for i in 1: nqubits], 1.0)
+# for noise_rate in 0:0.02:0.2
+#     #生成一排T gate的线路
+#     circuit3=one_layer_of_T_gate(nqubits;noise_rate,noise_type = 3)
+#     # # Plot the Third dataset
+#     Plot_transition_point!(pic3, circuit3, observable3, parameters3; label="p= $noise_rate")
+# end
+# display(pic3)
+##############################################################################
+
+# #生成一排Clifford+ T gate的线路
+# circuit_cliffordT=noisy_Clifford_T_circuit(nqubits,nlayers;topology = nothing,noise_rate = 0.2)
+# #确定最终要测量的Pauli算符
+# observable_cliffordT = PauliSum(nqubits)
+# add!(observable_cliffordT, [:X for i in 1:nqubits], [i for i in 1: nqubits], 1.0)
+# parameters_cliffordT =[]
+# # # Plot the Third dataset
+# Plot_transition_point!(pic, circuit_cliffordT, observable_cliffordT, parameters_cliffordT; label="Clifford+T circuit")
+
+##############################################################################
+# #生成随机一排2-qubit unitary拼成的线路circuit1
+# circuit1 = su4circuit(nqubits, nlayers; topology=topology)
+# nparams = countparameters(circuit1)
+# print(nparams)
+# parameters1 = rand_para * dt
+# #确定最终要测量的Pauli算符
+# observable1 = PauliSum(nqubits)
+# # add!(observable1, [:X for i in 1:nqubits], [i for i in 1:nqubits], 1.0)
+# add!(observable1, [:X], [7], 1.0)
+# # # Plot the first dataset
+# Plot_transition_point!(pic, circuit1, observable1, parameters1; label="Noiseless circuit")
+# ##############################################################################
+#生成另一排含噪声2-qubit unitary拼成的线路circuit2
+#改变noise的大小
+pic = plot(title="su(4)+Depolarizing noise", xlabel=L"\alpha", ylabel=L"C(\alpha)", yscale=:log10,legend=:outerright)
+for noise_rate in 0:0.01:0.1
+    local circuit2 = noisy_su4circuit(nqubits, nlayers; topology=topology,noise_rate = noise_rate,noise_type = 3)
+    local nparams = countparameters(circuit2)
+    local rand_para = rand(nparams)
+    local parameters2 = rand_para * dt
+    #确定最终要测量的Pauli算符
+    local observable2 = PauliSum(nqubits)
+    # add!(observable2, [:X for i in 1:nqubits], [i for i in 1:nqubits], 1.0)
+    add!(observable2, [:X], [7], 1.0)
+    # # Plot the second dataset
+    Plot_transition_point!(pic, circuit2, observable2, parameters2; label="Noisy circuit, p= $noise_rate")
+end
 display(pic)
+# ############################################################################
+
+
+# ##############################################################################
+# #生成另一排含噪声2-qubit unitary拼成的线路circuit2
+# #改变layer的大小
+# for nlayers in 1:4:28
+#     local circuit2 = noisy_su4circuit(nqubits, nlayers; topology=topology,noise_rate = 0.02)
+#     local nparams = countparameters(circuit2)
+#     local rand_para = rand(nparams)
+#     local parameters2 = rand_para * dt
+#     #确定最终要测量的Pauli算符
+#     local observable2 = PauliSum(nqubits)
+#     # add!(observable2, [:X for i in 1:nqubits], [i for i in 1:nqubits], 1.0)
+#     add!(observable2, [:X], [7], 1.0)
+#     # # Plot the second dataset
+#     Plot_transition_point!(pic, circuit2, observable2, parameters2; label="Noisy circuit, layer number= $nlayers")
+# end
+# ############################################################################
+# # Display the plot
+
 
 
 
--- plot_if_i.jl
+++ plot_if_i.jl
@@ -4,25 +4,46 @@
 using PauliPropagation
 using Plots
 using Random
-
-Random.seed!(1234)
+function noisy_circuit(nqubits::Integer, nlayers::Integer;noise_rate::Real)
+    """
+    su4circuit(nqubits::Integer, nlayers::Integer; topology=nothing)
+    
+    Create a circuit that consists of layers of SU(4) gates on a given topology. 
+    SU(4) gates are decomposed via the KAK Decomposition into single-qubit Z-X-Z gates on each qubit, followed by XX-YY-ZZ gates and again single-qubit Z-X-Z gates, for a total of 15 Pauli gates.
+    A topology can be specified as a list of pairs of qubit indices. If no topology is specified, a bricklayer topology is used.
+    """
+        circuit::Vector{Gate} = []
+        for nl in 1:nlayers
+            append!(circuit, (FrozenGate(DepolarizingNoise(qind), noise_rate) for qind in 1:nqubits))
+        end 
+        return circuit
+    end
 nqubits = 5
-alpha = 1.5
+nlayers = 1
+circuit = noisy_circuit(nqubits, nlayers; noise_rate=0.1)
 observable = PauliSum(nqubits)
 add!(observable, [:Y,:Z,:X], [1,2,3], 1.0)
-nlayers = 50
-topology = bricklayertopology(nqubits; periodic=true)
-circuit =su4circuit(nqubits, nlayers; topology=topology)
-dt = 3.14159265358979323846
-nparams = countparameters(circuit)
-parameters = rand(nparams) * dt
-pauli_sum = propagate(circuit, observable, parameters)
-println(pauli_sum)
-my_values = [x^2 for x in collect(values(pauli_sum.terms))]
-println("sum_of_values=", sum(my_values))
-my_length = length(my_values)
-println("my_length=", my_length)
-sorted_my_values = sort(my_values, rev=true)
+pauli_sum = propagate(circuit, observable)
+
+# Random.seed!(1234)
+# alpha = 1.5
+
+# nlayers = 50
+# topology = bricklayertopology(nqubits; periodic=true)
+# circuit =su4circuit(nqubits, nlayers; topology=topology)
+# dt = 3.14159265358979323846
+# nparams = countparameters(circuit)
+# parameters = rand(nparams) * dt
+# pauli_sum = propagate(circuit, observable, parameters)
+# println(pauli_sum)
+# my_values = [x^2 for x in collect(values(pauli_sum.terms))]
+# println("sum_of_values=", sum(my_values))
+# my_length = length(my_values)
+# println("my_length=", my_length)
+# sorted_my_values = sort(my_values, rev=true)
+
+
+
 # range = 1:1
 # Alist = []
 # for r in range
@@ -38,15 +59,15 @@ sorted_my_values = sort(my_values, rev=true)
 # scatter(range, Alist, title="A vs r", xlabel="r", ylabel="A", label="Data",yscale=:log10)
 # # plot(range, Alist, title="A vs nlayers", xlabel="nlayers", ylabel="A", label="Data")
 # Plot for r = 0
-r = 0
-A_values = [(i^r)*sorted_my_values[i] for i in 1:my_length]
-global pic = plot(1:my_length, A_values, title="function vs i", xlabel="i", ylabel="i^r * f", label="r = 0")
+# r = 0
+# A_values = [(i^r)*sorted_my_values[i] for i in 1:my_length]
+# global pic = plot(1:my_length, A_values, title="function vs i", xlabel="i", ylabel="i^r * f", label="r = 0")
 
-for r in 0.1:0.1:1.5
-    local f = [(i^r)*sorted_my_values[i] for i in 1:my_length]
-    plot!(pic,1:my_length, f, label="r =$(r)")
-end
-display(pic)
+# for r in 0.1:0.1:1.5
+#     local f = [(i^r)*sorted_my_values[i] for i in 1:my_length]
+#     plot!(pic,1:my_length, f, label="r =$(r)")
+# end
+# display(pic)
 # # Plot for r = 2
 # for r in 0.1:0.1
 #     f = [(i^r)*sorted_my_values[i] for i in 1:my_length]
